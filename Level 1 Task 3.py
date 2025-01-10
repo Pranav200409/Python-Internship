20d6e0e67d544895512cc0f7f5d45d28e10ab9fa
Pranav Patil <67407549+Pranav200409@users.noreply.github.com>
2025-01-10 20:01:13 +0530

    Level 1 Task 3.py

--- Level 1 Task 3.py
+++ Level 1 Task 3.py
@@ -1,3 +1,6 @@
+'''Develop a Python function that validates whether a given string is a valid email address.
+Implement checks for the format, including the presence of an "@" symbol and a domain name'''
+
 import re
 def valid_email(email):
     email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
